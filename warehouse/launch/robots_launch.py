09c20fed043cc72d9700480ccb07bf79f8964af5
Parv-Maheshwari <parvmaheshwari2002@gmail.com>
2021-06-01 19:00:56 +0530

    launch file parametrised over number of robots

---
+++ warehouse/launch/robots_launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+import yaml
+
+def generate_launch_description():
+
+    config = os.path.join(
+    get_package_share_directory('warehouse'),
+    'config',
+    'task_allocator.yaml'
+    )
+    print(config)
+    with open(config) as file:
+        documents = yaml.full_load(file)
+        print(documents)
+        robot_no = documents['task_allocator']['ros__parameters']['robot_no']
+        print(robot_no)
+    robots_node =[]
+    for i in range(robot_no):
+        robots_node.append(
+        Node(
+            package='warehouse',
+            executable='robots',
+            name=f'robot{i}',
+            output='screen',
+            emulate_tty=True,
+            parameters=[
+                {'my_parameter': f'{i}'}
+            ]
+        ))
+    return LaunchDescription(robots_node)
