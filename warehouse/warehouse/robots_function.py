4997e250e2fc5230c0186de4f52183544f0611da
Parv-Maheshwari <parvmaheshwari2002@gmail.com>
2021-06-11 00:29:55 +0530

    robot function nodes independent of "/orders"

--- warehouse/warehouse/robots_function.py
+++ warehouse/warehouse/robots_function.py
@@ -18,15 +18,13 @@ class robot(Node):
         self.my_param = self.get_parameter('my_parameter').get_parameter_value().string_value
 
         self.publisher_ = self.create_publisher(Robot, 'topic_pubs%s' % self.my_param, 10)
-        self.subscription = self.create_subscription(String,'orders',self.listener_callback,10)
         
         self.i =0
-        self.subscription
-        # timer_period = 2  # seconds
-        # self.timer = self.create_timer(timer_period, self.timer_callback)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
         # self.declare_parameter('my_parameter', 'world')
 
-    def listener_callback(self,msg):
+    def timer_callback(self):
         # self.get_logger().info(f'Hello. Robot has to go from {msg.shelf_no} to {msg.picking_st_no}')
         msg1 = Robot()
         msg1.pos.x =float(self.my_param)
